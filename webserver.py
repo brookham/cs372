76dbd8e64150b541c24255ddec1ca7ccd813be2f
brookham <bhamilton3264@gmail.com>
2025-10-06 08:14:24 -0700

    tried to fix

--- webserver.py
+++ webserver.py
@@ -15,10 +15,15 @@ Content-Length: 6 \r\n\
 Connection: close\r\n\r\nHello!"
 
 
+
 while True:
     new_conn = s.accept()
     new_socket = new_conn[0]
+    d = new_socket.recv(1000).decode()
+
     print(new_conn[1]) #see if client was conencting properly
     new_socket.sendall(res.encode())
+    if d == "\r\n\r\n":
+        break
   
     new_socket.close()
